978a8e6a055b1e1db1fc0c138c4d6822c2477abb
Maciek <maciej.harazinski@gmail.com>
2016-11-09 20:23:30 +0100

    day 4

--- src/hr_01/Output.java
+++
@@ -1,9 +0,0 @@
-package hr_01;
-
-public class Output {
-    String print(String data) {
-        String hello = "Hello world\n";
-        return hello.concat(data);
-    }
-
-}
---
+++ src/hr_04/Hr04.java
@@ -0,0 +1,25 @@
+package hr_04;
+
+import java.util.Scanner;
+
+/**
+ * Created by Maciek on 09.11.2016.
+ */
+public class Hr04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int age = sc.nextInt();
+            Person p = new Person(age);
+            p.amIOld();
+
+            for (int j = 0; j < 3; j++) {
+                p.yearPasses();
+            }
+            p.amIOld();
+            System.out.println();
+        }
+        sc.close();
+    }
+}
