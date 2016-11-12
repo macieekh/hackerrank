12d58077df85d3ae7bce76dda3c2e14a598c1ec7
Maciek <maciej.harazinski@gmail.com>
2016-11-12 21:23:24 +0100

    day 7

--- src/hr_07/Hr07.java
+++ src/hr_07/Hr07.java
@@ -1,8 +1,24 @@
 package hr_07;
 
-/**
- * Created by Maciek on 12.11.2016.
- */
-public class Hr07ls
+import java.util.Scanner;
+
+public class Hr07
 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+        in.close();
+        backward(arr);
+    }
+
+    public static void backward(int[] arg) {
+            for (int i = arg.length-1; i >=0 ; i--) {
+            System.out.print(arg[i] + " ");
+        }
+    }
+
 }
