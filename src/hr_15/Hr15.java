b87d9b640ce23162610bc06c2a322198b12e5388
Maciek <maciej.harazinski@gmail.com>
2016-11-21 18:20:50 +0100

    day 15

---
+++ src/hr_15/Hr15.java
@@ -0,0 +1,54 @@
+package hr_15;
+
+
+import java.util.Scanner;
+import java.util.jar.Pack200;
+
+public class Hr15 {
+
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+
+    public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static Node insert(Node head,int data) {
+        //Node temp;
+        Node temp = head;
+        Node lastnode = new Node(data);
+        if (temp == null) {
+            head = lastnode;
+        } else {
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = lastnode;
+        }
+        /*}
+        else {
+            while (head.next != null) {
+                temp = temp.next;
+            }
+            Node lastnode = new Node (data);
+            temp.next = lastnode;
+        }*/
+
+        return head;
+    }
+}
---
+++ src/hr_15/Node.java
@@ -0,0 +1,11 @@
+package hr_15;
+
+public class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
