135e1375aaf07e9d5712c63010de0b7f31234e74
Maciek <maciej.harazinski@gmail.com>
2016-11-22 18:39:54 +0100

    day 20

---
+++ src/hr_20/Hr20.java
@@ -0,0 +1,14 @@
+package hr_20;
+
+
+import java.util.Scanner;
+
+public class Hr20 {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int a[] = new int[n];
+    for(int a_i=0; a_i < n; a_i++){
+        a[a_i] = in.nextInt();
+    }
+
+}
